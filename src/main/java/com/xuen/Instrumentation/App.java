0bbcc5025746e474e8cc99bb2644c83b4cd44bdb
xuzheng <xuzheng@xuzhengdeMacBook-Pro.local>
2017-11-13 22:55:50 +0800

    Initial commit

---
+++ src/main/java/com/xuen/Instrumentation/App.java
@@ -0,0 +1,10 @@
+package com.xuen.Instrumentation;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
