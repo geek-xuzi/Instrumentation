556c2adc57aabbd70d659398aa8beae64b5eaa87
xuzheng <1090338771@qq.com>
2017-11-13 23:27:38 +0800

    完成mainfest.mf文件的配置

---
+++ src/main/java/com/xuen/Instrumentation/Agent.java
@@ -0,0 +1,13 @@
+package com.xuen.Instrumentation;
+
+import java.lang.instrument.Instrumentation;
+
+public class Agent {
+
+
+    public static void premain(String args, Instrumentation instrumentation) {
+        System.out.println(args);
+    }
+}
+
+
